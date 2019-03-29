98db6b157d66d5f3a259e8e70b82a0f1a00757e7
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2019-03-29 14:49:10 +0900

    Create Monitor-PIWebAPI.ps1
    Initial version of the script

---
+++ Monitor-PIWebAPI.ps1
@@ -0,0 +1,26 @@
+﻿# Retrive the last crawl date of a PI Data Archive
+function Get-Data([string]$username, [string]$password, [string]$url) {
+  $credPair = "$($username):$($password)"
+  $encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credPair))
+  $headers = @{ Authorization = "Basic $encodedCredentials" }
+  $responseData = Invoke-WebRequest -Uri $url -Method Get -Headers $headers -UseBasicParsing
+  return $responseData
+}
+$piwebapihost = "https://devdata.osisoft.com/"
+$pi = "PISRV1"
+
+$url = $piwebapihost + "piwebapi/search/sources?name=pi:" + $piwebapihost
+$dataRaw = Get-Data -username webapiuser -password !try3.14webapi! -url https://devdata.osisoft.com/piwebapi/search/sources?name=pi:PISRV1
+
+$json = $dataRaw | ConvertFrom-Json
+$lastCrawl = $json.Items[0].LastCrawl
+
+Write-Host Last crawl date: $lastCrawl
+
+#Convert and store the value to store in tag with datetime type
+$format = 'yyyy-MM-ddThh:mm:ss.fffffffZ'
+$value = ([datetime]::parseexact($lastCrawl, 'yyyy-MM-ddThh:mm:ss.fffffffZ', $null)).ToUniversalTime()
+
+$pi = Connect-PIDataArchive -PIDataArchiveMachineName masterpi
+$tag = Get-PIPoint -Connection $pi -Name PIWebAPIStatus2
+Add-PIValue -PIPoint $tag -Value $value -Time (ConvertFrom-AFRelativeTime -RelativeTime *)
